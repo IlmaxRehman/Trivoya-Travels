d22eb8632f2d5bc9170d51e8023791b30273d674
IlmaxRehman <ilmarehman022@gmail.com>
2026-03-25 13:47:57 +0530

    Added tour model and admin panel

--- backend/tours/models.py
+++ backend/tours/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    duration = models.CharField(max_length=50)
+    price = models.CharField(max_length=50)
+
+    description = models.TextField()
+
+    itinerary = models.TextField()
+
+    hero_image = models.URLField()
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return self.name
